81502b26d02ba01ce290c7520e57cd2ab80768e9
kjetilhammerseth <kjetilhammerseth@gmail.com>
2019-02-17 15:57:46 +0100

    Init Commit 2

--- src/main/java/com/utstyrwebapp/demo/UtstyrWebAppApplication.java
+++ src/main/java/com/utstyrwebapp/demo/UtstyrWebAppApplication.java
@@ -9,12 +9,15 @@ public class UtstyrWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UtstyrWebAppApplication.class, args);
+
         System.out.println("** Utstyr Web App started**");
 
         // Initialize database and connect
         DbAdapter dbAdapter = new DbAdapter();
         dbAdapter.connect();
-        dbAdapter.disconnect();
+        // dbAdapter.disconnect();
+
+
     }
 
 }
